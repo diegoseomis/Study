7079a42d581c581ac21ef9bd14282620987b1b7b
diegoseomis <68360764+diegoseomis@users.noreply.github.com>
2021-07-16 18:09:15 -0300

    Exercícios
    tabuada

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
+function Tabuada(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    
+    if(num.value.length == 0){
+        window.alert('Por favor, digite um número!')
+        
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML= ''
+        while (c<=10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c} `
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
 
-function run(){
-    var msg = document.getElementById('msg')
-    var img = document.getElementById('image')
-    var data = new Date()
-    var hour = data.getHours()
-    msg.innerHTML = `Agora são ${hour} horas.`
-   
-    if (hour >= 0 && hour < 12){
-        //BOM DIA
-        img.src = 'morning.png'
-        document.body.style.background = '#f1df99'
-    }else if (hour >= 12 && hour < 18){
-        //BOA TARDE
-        img.src ='afternoon.png'
-        document.body.style.background = '#cd7015'
-    }else{
-        //BOA NOITE
-        img.src ='night.png'
-        document.body.style.background='#282828'
+
+        }
     }
-} 
+    
+}
